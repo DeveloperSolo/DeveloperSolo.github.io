a1cfed48690b5290a06d29359961d062db5e1ab6
DeveloperSolo <dragonloversol@gmail.com>
2024-01-22 01:47:28 +0800

    added experiences timeline (foundation only)

--- main.js
+++ main.js
@@ -22,6 +22,209 @@ function TryResetNavbarButton(event) {
 
 //#endregion Navbar
 
+//#region Experiences
+
+var experienceDictionary = [];
+
+var experienceFilterBitField = 1;
+const filterBitField = 
+[
+    "project",
+    "company",
+    // "industry",
+    // "academic",
+];
+var useAlternateAlignment = true;
+
+function UpdateExperiences()
+{
+    var timelineElements = document.querySelector("#experiences .timeline").children;
+    var activeIndex = 0;
+
+    for(var i = 2; i < timelineElements.length - 1; ++i)
+    {
+        var timelineNode = timelineElements[i];
+
+        var isEnabled = true;
+
+        if(experienceFilterBitField > 1)
+        {
+            var index = timelineNode.id;
+            var data = experienceDictionary[index];
+            var typeList = data["type"];
+            var typeBitField = 0;
+            for(var t = 0; t < typeList.length; ++t)
+            {
+                var type = typeList[t];
+                var typeIndex = filterBitField.indexOf(type) + 1;
+                typeBitField |= (1 << typeIndex);
+            }
+            isEnabled = Boolean(typeBitField & experienceFilterBitField);
+        }
+
+        if(isEnabled)
+        {
+            timelineNode.classList.remove("timeline-hidden");
+            if(useAlternateAlignment)
+            {
+                timelineNode.classList.add((activeIndex % 2) ? "timeline-right" : "timeline-left");
+                timelineNode.classList.remove((activeIndex % 2) ? "timeline-left" : "timeline-right");
+            }
+            else
+            {
+                timelineNode.classList.add("timeline-right");
+                timelineNode.classList.remove("timeline-left");
+            }
+
+            ++activeIndex;
+        }
+        else
+        {
+            timelineNode.classList.add("timeline-hidden");
+        }
+    }
+}
+
+function PopulateExperiences()
+{
+    var grid = document.querySelector("#experiences .timeline");
+    var template = grid.querySelector("#timeline-element-template");
+    var index = 0;
+
+    fetch("./data_experiences.json").then((res) => 
+    {
+        return res.json();
+    })
+    .then((data) => 
+    {
+        fetch("./data_experiences.json").then((res) => 
+        {
+            return res.json();
+        })
+        .then((data) => 
+        {
+            experienceDictionary = data;
+            for(var key in data)
+            {
+                var experienceData = data[key];
+                var typeList = experienceData["type"];
+                var title = experienceData["title"];
+                var subtitle = experienceData["subtitle"];
+                var date = experienceData["date"];
+                var imagePath = experienceData["imagePath"];
+
+                var instance = template.content.cloneNode(true);
+    
+                var container = instance.querySelector(".timeline-container");
+                container.id = key;
+                for(var i = 0; i < typeList.length; ++i)
+                {
+                    container.classList.add("timeline-" + typeList[i].toLowerCase());
+                }
+        
+                instance.querySelector(".timeline-title").textContent = title;
+                instance.querySelector(".timeline-subtitle").textContent = subtitle;
+                instance.querySelector(".timeline-date").textContent = date;
+                instance.querySelector("img").setAttribute("src", imagePath);
+                // instance.querySelector("a").setAttribute("href", "project-page.html?id=" + key);
+
+                grid.insertBefore(instance, grid.childNodes[grid.childNodes.length - 2]);
+                ++index;
+            }
+            UpdateExperiences();
+        });
+    });
+}
+
+function SetUseExperienceAlignment(value)
+{
+    if(useAlternateAlignment == value)
+    {
+        return;
+    }
+    useAlternateAlignment = value;
+    UpdateExperiences();
+}
+
+function UpdateFilterButtonDisplay()
+{
+    var container = document.querySelector(".timeline-filter");
+    for(var i = 0; i < container.children.length; ++i)
+    {
+        var button = container.children[i];
+        var isEnabled = Boolean(experienceFilterBitField & (1 << i));
+        if(isEnabled)
+        {
+            button.classList.add("timeline-filter-button-active");
+        }
+        else
+        {
+            button.classList.remove("timeline-filter-button-active");
+        }
+    }
+}
+
+function FilterOnClick(button)
+{
+    var buttonIndex = Array.from(button.parentElement.children).indexOf(button);
+    if(experienceFilterBitField & (1 << buttonIndex))
+    {
+        experienceFilterBitField = 1;
+    }
+    else
+    {
+        experienceFilterBitField = (1 << buttonIndex);
+    }
+    UpdateFilterButtonDisplay();
+    UpdateExperiences();
+}
+
+//#endregion Experiences
+
+//#region Awards
+
+class Award
+{
+    constructor(title, text)
+    {
+        this.title = title;
+        this.text = text;
+    }
+}
+
+const awards =
+[
+    new Award("Provost's List"),
+    new Award("Director's List"),
+    new Award("WorldSkills Bronze"),
+    new Award("Skills Mastery Award"),
+]
+
+function PopulateAwards()
+{
+    var grid = document.querySelector("#awards-grid");
+    var template = grid.querySelector("#awards-grid-item-template");
+
+    awards.forEach(award =>
+    {
+        var instance = template.content.cloneNode(true);
+
+        instance.querySelector(".award-name").textContent = award.title;
+        
+        grid.appendChild(instance);
+    });
+}
+
+//#endregion Awards
+
+//#region Main
+
+window.onload = function()
+{
+    PopulateExperiences();
+    UpdateFilterButtonDisplay();
+};
+
 window.onclick = function(event)
 {
     TryResetNavbarButton(event);
@@ -29,8 +232,20 @@ window.onclick = function(event)
 
 window.onresize = function()
 {
-    if(window.innerWidth >= 800)
+    if(window.innerWidth >= 1000)
     {
         ResetNavbarButton();
+        SetUseExperienceAlignment(true);
+    }
+    else
+    {
+        SetUseExperienceAlignment(false);
     }
 }
+
+function LogTest()
+{
+    console.log("Testing");
+}
+
+//#endregion Main
